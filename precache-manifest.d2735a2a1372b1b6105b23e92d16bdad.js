8ce5707f660c409a70a4763cbb41d67f115937f1
nishan666 <nishankumar559@gmail.com>
2023-06-12 21:28:50 +0530

    Updates

---
+++ precache-manifest.d2735a2a1372b1b6105b23e92d16bdad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da172d5ceac9e96f4c36184c913b623e",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "e933823b1a2a95785de5",
+    "url": "/emojipedia/static/js/2.9dc37305.chunk.js"
+  },
+  {
+    "revision": "0612de1de8a0756a003d",
+    "url": "/emojipedia/static/js/main.a22625c4.chunk.js"
+  },
+  {
+    "revision": "3ad02f26d7c433e7efca",
+    "url": "/emojipedia/static/js/runtime-main.d1c65f91.js"
+  }
+]);
